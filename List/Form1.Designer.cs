b095bdf090d457d0b7245fc39ecabf2d5cfa6f7a
Büşra <ucarbusra6@gmail.com>
2023-06-22 16:58:38 +0300

    Add project files.

---
+++ List/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace List
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxA = new System.Windows.Forms.ListBox();
+            this.listBoxB = new System.Windows.Forms.ListBox();
+            this.listBoxC = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxA
+            // 
+            this.listBoxA.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.listBoxA.FormattingEnabled = true;
+            this.listBoxA.ItemHeight = 16;
+            this.listBoxA.Location = new System.Drawing.Point(77, 48);
+            this.listBoxA.Name = "listBoxA";
+            this.listBoxA.Size = new System.Drawing.Size(124, 260);
+            this.listBoxA.TabIndex = 0;
+            // 
+            // listBoxB
+            // 
+            this.listBoxB.FormattingEnabled = true;
+            this.listBoxB.ItemHeight = 16;
+            this.listBoxB.Location = new System.Drawing.Point(380, 48);
+            this.listBoxB.Name = "listBoxB";
+            this.listBoxB.Size = new System.Drawing.Size(124, 260);
+            this.listBoxB.TabIndex = 1;
+            // 
+            // listBoxC
+            // 
+            this.listBoxC.FormattingEnabled = true;
+            this.listBoxC.ItemHeight = 16;
+            this.listBoxC.Location = new System.Drawing.Point(721, 48);
+            this.listBoxC.Name = "listBoxC";
+            this.listBoxC.Size = new System.Drawing.Size(124, 260);
+            this.listBoxC.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(95, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ListeA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label2.Location = new System.Drawing.Point(404, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "ListeB";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(754, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "ListeC";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(12, 314);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(285, 29);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "A Listesinde bütün kişiler yer alır";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.Color.Green;
+            this.button2.Location = new System.Drawing.Point(303, 314);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(340, 29);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "B Listesinde 30 yaş ve üzeri kişiler yer alır";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button3.Location = new System.Drawing.Point(649, 314);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(324, 29);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "C Listesinde 30 yaş altı kişiler yer alır";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(985, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBoxC);
+            this.Controls.Add(this.listBoxB);
+            this.Controls.Add(this.listBoxA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxA;
+        private System.Windows.Forms.ListBox listBoxB;
+        private System.Windows.Forms.ListBox listBoxC;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ List/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace List
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var ListeA = new List<string>() { "Ayşe-33", "Ali Hakan-28", "Fatma Nur-40", "Numan-27","Büşra-27","Pınar-30","Mustafa Deniz-35","Ayça-18","Barış-45" ,"Mehmet-32"};
+            var ListeB = new List<string>();
+            var ListeC = new List<string>();
+            var yas = 30;
+
+            for (int i = 0; i < ListeA.Count; i++)
+            {
+                var a = ListeA[i].Split('-');
+                int b = Convert.ToInt16(a[1]);
+
+                if (b >= yas)
+                {
+                    ListeB.Add(ListeA[i]);
+                }
+                else
+                {
+                    ListeC.Add(ListeA[i]);
+                }
+            }
+
+            foreach (var item in ListeA)
+            {
+                listBoxA.Items.Add(item);
+            }
+
+            foreach (var item in ListeB)
+            {
+                listBoxB.Items.Add(item);
+            }
+
+            foreach (var item in ListeC)
+            {
+                listBoxC.Items.Add(item);
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
